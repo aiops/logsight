f991651563455efab7bef11d87b5383a57b886e2
Petar Ilijevski <petar.ilijevski@campus.tu-berlin.de>
2021-11-24 16:47:05 +0100

    first commit including database

---
+++ utils/helpers.py
@@ -0,0 +1,18 @@
+from collections import Iterable
+
+
+def unpack_singleton(x):
+    """Gets the first element if the iterable has only one value.
+
+    Otherwise return the iterable.
+
+    Parameters
+    ----------
+        x: iterable
+    Returns
+    --------
+        The same iterable or the first element.
+    """
+    if (hasattr(x, "__iter__") or isinstance(x, Iterable)) and len(x) == 1:
+        return unpack_singleton(x[0])
+    return x
