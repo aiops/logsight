bc7c26248b8def00d8ba593932edc67657033874
Alexander Acker <alexander.acker@tu-berlin.de>
2022-02-09 20:56:29 +0100

    Added app id to RPC response

--- logsight/logsight_classes/responses.py
+++ logsight/logsight_classes/responses.py
@@ -4,16 +4,16 @@ from pydantic import BaseModel
 
 
 class Response(BaseModel):
-    timestamp: datetime.datetime = datetime.datetime.now().isoformat()
+    id: str
     message: str
     status: HTTPStatus
 
 
 class ErrorResponse(Response):
-    def __init__(self, message, status = HTTPStatus.INTERNAL_SERVER_ERROR):
-        super(ErrorResponse, self).__init__(message=message, status=status)
+    def __init__(self, id, message, status = HTTPStatus.INTERNAL_SERVER_ERROR):
+        super(ErrorResponse, self).__init__(id=id, message=message, status=status)
 
 
 class SuccessResponse(Response):
-    def __init__(self, message):
-        super(SuccessResponse, self).__init__(message=message, status=HTTPStatus.OK)
+    def __init__(self, id, message):
+        super(SuccessResponse, self).__init__(id=id, message=message, status=HTTPStatus.OK)
