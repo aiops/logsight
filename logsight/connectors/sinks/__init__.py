162c4b459f3318210ba59144b9b2ca4515391ff3
Alexander Acker <alexander.acker@tu-berlin.de>
2022-02-21 11:43:58 +0100

    kafka source import fix (#14)

--- logsight/connectors/sinks/__init__.py
+++ logsight/connectors/sinks/__init__.py
@@ -4,7 +4,6 @@ from .socket import SocketSink
 from .queue import SinkQueue
 from .file import FileSink
 from .multi import MultiSink
-from .kafka import KafkaSink
 from .elasticsearch import ElasticsearchSink
 from .empty import EmptySink
 from .zeromq import ZeroMQPubSink
