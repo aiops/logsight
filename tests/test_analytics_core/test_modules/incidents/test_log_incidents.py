56ec99274543d8093a46186bcfe929a40f6dda14
snedelkoski <sashonedelkoski@gmail.com>
2022-06-08 12:16:12 +0200

    fix test incidents

--- tests/test_analytics_core/test_modules/incidents/test_log_incidents.py
+++ tests/test_analytics_core/test_modules/incidents/test_log_incidents.py
@@ -4,5 +4,6 @@ from tests.inputs import processed_logs, incident_results
 
 def test_calculate_incidents():
     detector = IncidentDetector()
-    result = detector.calculate_incidents(processed_logs)
+    result = detector.calculate_incidents(processed_logs, [])
+    print(result)
     assert result == incident_results
