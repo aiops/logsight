b0ebf4238aa44e95438366185f50dda6b93ae8f5
Sasho Nedelkoski <sashonedelkoski@gmail.com>
2022-06-08 13:18:28 +0200

    improving incidents for the new ui (#92)
    * improving incidents for the new ui
    
    * fix test incidents
    
    * fix incident test
    
    * logging to info
    
    * formatting and removing prints

--- tests/test_analytics_core/test_modules/incidents/test_log_incidents.py
+++ tests/test_analytics_core/test_modules/incidents/test_log_incidents.py
@@ -4,5 +4,5 @@ from tests.inputs import processed_logs, incident_results
 
 def test_calculate_incidents():
     detector = IncidentDetector()
-    result = detector.calculate_incidents(processed_logs)
+    result = detector.calculate_incidents(processed_logs, [])
     assert result == incident_results
