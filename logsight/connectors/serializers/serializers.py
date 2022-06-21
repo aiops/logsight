8ca4277c1d2fcbd8c9e90fc0453ce79eaf48f3c4
Alexander Acker <alexander.acker@tu-berlin.de>
2022-06-21 10:43:04 +0200

    Renamed JSONSerializer to JSONStringSerializer. Fixed LogBatchSerializer docs comments.

--- logsight/connectors/serializers/serializers.py
+++ logsight/connectors/serializers/serializers.py
@@ -1,35 +1,51 @@
+from abc import abstractmethod
 from dataclasses import asdict
+from typing import Any
 
 import ujson
 
 from analytics_core.logs import LogBatch, LogsightLog
-from connectors.serializers.base import LogBatchSerializer
+from connectors.serializers import Serializer
 
 
-class JSONSerializer(LogBatchSerializer):
+class LogBatchSerializer(Serializer):
     """
-    Transformer class for transforming data to LogBatch
+    Interface for serialization of LogBatch objects.
+    """
+
+    @abstractmethod
+    def serialize(self, data: LogBatch) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
+    def deserialize(self, data: Any) -> LogBatch:
+        raise NotImplementedError
+
+
+class JSONStringSerializer(LogBatchSerializer):
+    """
+    Serializer class for transforming JSON strings to LogBatch
     """
 
     def serialize(self, data: LogBatch) -> str:
         """
-        The serialize function takes a LogBatch object and returns a byte string.
-        The byte string is the serialized representation of the LogBatch object.
+        The serialize function takes a LogBatch object and returns a JSON string.
+        The JSON string is the serialized representation of the LogBatch object.
 
         Args:
             data:LogBatch: Pass the data to be serialized
 
         Returns:
-            A bytes object
+            A JSON string
         """
         return ujson.dumps(asdict(data))
 
     def deserialize(self, data: str) -> LogBatch:
         """
-        The deserialize function takes a byte string and returns an instance of the LogBatch class.
+        The deserialize function takes a JSON string and returns an instance of the LogBatch class.
 
         Args:
-            data:bytes: Pass the data to be deserialized
+            data:string: Pass the data to be deserialized
 
         Returns:
             A LogBatch object
